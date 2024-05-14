1e908fe4e8cca47b9ff2cdb6b176ec58aeefdbac
hichemrouba <hichem.idenet@gmail.com>
2024-05-14 15:44:56 +0100

    Update vehicles check

--- src/pages/Vehicles_checks.tsx
+++ src/pages/Vehicles_checks.tsx
@@ -1,44 +1,38 @@
-import { Dropdown, Table } from "react-bootstrap";
+import { Button, Dropdown, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import { useTranslate } from "../components/LanguageProvider";
 import { useState } from "react";
 
-export function Vehicleschecks() {
-  const { translate } = useTranslate();
-  const [currentPage, setCurrentPage] = useState(0);
-  const itemsPerPage = 5;
-  const [visibleColumns, setVisibleColumns] = useState([
-    "creationDate",
-    "auditor",
-    "outgoingDriver",
-    "incomingDriver",
-    "immatriculation",
-    "maintenance",
-    "actions",
-  ]);
 
+
+export function Vehicleschecks() {
+
+  const { translate } = useTranslate();
+  const [currentPage, setCurrentPage] = useState(0); // État pour suivre la page actuelle
+  const itemsPerPage = 5; // Nombre d'éléments par page
+
+
+
+  // Données statiques
   const staticData = [
-    { id: 1, creationDate: "2024-05-12", auditor: "John Doe", outgoingDriver: "Alice", incomingDriver: "Bob", immatriculation: "XYZ123", maintenance: "Scheduled", actions: "Edit/Delete" },
-    // Ajoutez d'autres données ici...
+    { id: 1, DateCréation: "2024-05-12", Vérificateur: "John Doe", Nom_Chaffeur_Sortant: "Alice", Nom_Chaffeur_Entrant: "Bob", immatriculation: "XYZ123", maintenance: "Scheduled", actions: "Edit/Delete" },
+    { id: 2, DateCréation: "2024-05-13", Vérificateur: "Jane Smith", Nom_Chaffeur_Sortant: "Eve", Nom_Chaffeur_Entrant: "Charlie", immatriculation: "ABC456", maintenance: "Unscheduled", actions: "Edit/Delete" },
+    { id: 3, DateCréation: "2024-05-14", Vérificateur: "Michael Johnson", Nom_Chaffeur_Sortant: "Grace", Nom_Chaffeur_Entrant: "David", immatriculation: "DEF789", maintenance: "Scheduled", actions: "Edit/Delete" },
+    { id: 4, DateCréation: "2024-05-15", Vérificateur: "Sarah Williams", Nom_Chaffeur_Sortant: "Frank", Nom_Chaffeur_Entrant: "Emily", immatriculation: "GHI012", maintenance: "Unscheduled", actions: "Edit/Delete" },
+    { id: 5, DateCréation: "2024-05-16", Vérificateur: "Chris Brown", Nom_Chaffeur_Sortant: "Hannah", Nom_Chaffeur_Entrant: "George", immatriculation: "JKL345", maintenance: "Scheduled", actions: "Edit/Delete" },
+    { id: 6, DateCréation: "2024-05-17", Vérificateur: "Amanda Davis", Nom_Chaffeur_Sortant: "Isaac", Nom_Chaffeur_Entrant: "Olivia", immatriculation: "MNO678", maintenance: "Unscheduled", actions: "Edit/Delete" },
+    { id: 7, DateCréation: "2024-05-18", Vérificateur: "Jason Taylor", Nom_Chaffeur_Sortant: "Julia", Nom_Chaffeur_Entrant: "Kevin", immatriculation: "PQR901", maintenance: "Scheduled", actions: "Edit/Delete" },
+    { id: 8, DateCréation: "2024-05-19", Vérificateur: "Lisa Martinez", Nom_Chaffeur_Sortant: "Liam", Nom_Chaffeur_Entrant: "Natalie", immatriculation: "STU234", maintenance: "Unscheduled", actions: "Edit/Delete" },
+    { id: 9, DateCréation: "2024-05-20", Vérificateur: "Matthew Rodriguez", Nom_Chaffeur_Sortant: "Sophia", Nom_Chaffeur_Entrant: "Robert", immatriculation: "VWX567", maintenance: "Scheduled", actions: "Edit/Delete" },
+    { id: 10, DateCréation: "2024-05-21", Vérificateur: "Jennifer Wilson", Nom_Chaffeur_Sortant: "Tom", Nom_Chaffeur_Entrant: "Victoria", immatriculation: "YZA890", maintenance: "Unscheduled", actions: "Edit/Delete" }
   ];
+
 
   const handlePageClick = async (data: any) => {
     const selectedPage = data.selected;
-    setCurrentPage(selectedPage);
-    window.scrollTo(0, 0);
-  };
-
-  const toggleColumnVisibility = (columnName: string) => {
-    setVisibleColumns((prevColumns) => {
-      if (prevColumns.includes(columnName)) {
-        // Retirer la colonne si elle est déjà visible
-        return prevColumns.filter((col) => col !== columnName);
-      } else {
-        // Ajouter la colonne si elle n'est pas visible
-        return [...prevColumns, columnName];
-      }
-    });
+    setCurrentPage(selectedPage); // Mettre à jour la page actuelle
+    window.scrollTo(0, 0); // Fait défiler vers le haut de la page
   };
 
   // Calcule l'indice de début et de fin pour les éléments à afficher
@@ -46,6 +40,8 @@ export function Vehicleschecks() {
   const endIndex = startIndex + itemsPerPage;
   // Extrait les éléments correspondant à la page actuelle
   const currentItems = staticData.slice(startIndex, endIndex);
+
+
 
   return (
     <>
@@ -72,17 +68,20 @@ export function Vehicleschecks() {
                 <i className="fas fa-chevron-down" style={{ color: 'black' }}></i>
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item >test1</Dropdown.Item>
-                <Dropdown.Item >test2</Dropdown.Item>
-                <Dropdown.Item >test3</Dropdown.Item>
-                <Dropdown.Item >test4</Dropdown.Item>
+                <Dropdown.Item >Vérificateur</Dropdown.Item>
+                <Dropdown.Item >Nom_Chaffeur_Sortant</Dropdown.Item>
+                <Dropdown.Item >Nom_Chaffeur_Entrant</Dropdown.Item>
+                <Dropdown.Item >Immatriculation</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
             <input
               type="text"
               placeholder="test"
               className="form-control"
+            //onChange={}
+            //value={searchTerm}
             />
+
           </div>
         </div>
         <div className="col-md-8 d-flex justify-content-end align-items-center">
@@ -94,6 +93,7 @@ export function Vehicleschecks() {
                 aria-controls="DataTables_Table_0"
                 className="custom-select custom-select-sm form-control form-control-sm ml-2"
                 style={{ width: "66px" }}
+              //onChange={}
               >
                 <option value="15">15</option>
                 <option value="30">30</option>
@@ -107,51 +107,88 @@ export function Vehicleschecks() {
               {translate('entries')}
             </label>
           </div>
-          <Dropdown>
-          <Dropdown.Toggle variant="link" id="dropdown-basic">
-             Filtre
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              {Object.keys(staticData[0]).map((columnName) => (
-                <Dropdown.Item key={columnName} onClick={() => toggleColumnVisibility(columnName)}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      checked={visibleColumns.includes(columnName)}
-                      onChange={() => toggleColumnVisibility(columnName)}
-                    />
-                    {translate(columnName)}
-                  </label>
-                </Dropdown.Item>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
         </div>
         <div className="row m-2">
-          <Table >
-            <thead>
-              <tr>
-                {visibleColumns.map(column => (
-                  <th key={column}>{translate(column)}</th>
-                ))}
+          <Table>
+            <thead className="bg-white text-uppercase">
+              <tr className="ligth ligth-data">
+                <th>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                    />
+                    <label className="form-check-label"></label>
+                  </div>
+                </th>
+                <th>DateCréation</th>
+                <th>Vérificateur</th>
+                <th>Nom_Chaffeur_Sortant</th>
+                <th>Nom_Chaffeur_Entrant</th>
+                <th>immatriculation</th>
+                <th>maintenance</th>
+                <th>Actions</th>
               </tr>
             </thead>
-            <tbody>
-              {currentItems.map(data => (
-                <tr key={data.id}>
-                  {visibleColumns.map(column => (
-                    <td key={column}>  {(data as any)[column]} </td>
-                  ))}
+            <tbody key="#" className="ligth-body">
+              {currentItems.map((data) => (
+                <tr className={''} key={data.id}>
+                  <td>
+                    <div className="form-check form-check-inline">
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                      />
+                    </div>
+                  </td>
+                  <td>{data.DateCréation}</td>
+                  <td>{data.Vérificateur}</td>
+                  <td>{data.Nom_Chaffeur_Sortant}</td>
+                  <td>{data.Nom_Chaffeur_Entrant}</td>
+                  <td>{data.immatriculation}</td>
+                  <td>{data.maintenance}</td>
+                  <td>
+                    <div className="d-flex align-items-center list-action">
+                      <Link
+                        to={`#`}
+                        className="badge badge-success mr-2"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Détail"
+                      >
+                        <i className="fa fa-eye" style={{ fontSize: '1.2em' }}></i>
+                      </Link>
+                      <a
+                        className="badge bg-warning mr-2"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Delete"
+                        data-original-title="Delete"
+                      >
+                        <i className="ri-delete-bin-line mr-0" style={{ fontSize: '1.2em' }}></i>
+                      </a>
+                      <a
+                        className="badge bg-primary mr-2"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="download"
+                        data-original-title="download"
+                      >
+                        <i className="las la-download" style={{ fontSize: '1.2em' }}></i>
+                      </a>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </Table>
         </div>
+
         <ReactPaginate
           previousLabel={"previous"}
           nextLabel={"next"}
           breakLabel={"..."}
-          pageCount={2} // Vous devez utiliser 2 ici
+          pageCount={2} // Ici, vous devez utiliser 2
           marginPagesDisplayed={2}
           pageRangeDisplayed={3}
           onPageChange={handlePageClick}
@@ -169,4 +206,5 @@ export function Vehicleschecks() {
       </div>
     </>
   )
+
 }
