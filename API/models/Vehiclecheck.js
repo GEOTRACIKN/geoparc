93a25886b078294f19f7b5e0282d7d6c3a3c6202
hichemrouba <hichem.idenet@gmail.com>
2024-05-20 15:09:23 +0100

    Some update add translate
    add on api vehiclecheck

--- API/models/Vehiclecheck.js
+++ API/models/Vehiclecheck.js
@@ -1,15 +1,31 @@
 const db = require("../database");
 
 const Vehiclecheck = {
-    
+
   // Retrieve all Vehiclecheck User Id
   getAllUserId: (page, limit, id_user, callback) => {
     let sql;
 
     if (id_user == 1) {
-      sql = ``;
+      sql = `SELECT 
+      id_verif,
+      Creation_date,checker,
+      Driver_out,
+      Driver_in,
+      license_vhc,
+      maintenance FROM gp_verif_vehicule 
+      ASC LIMIT ${limit} OFFSET ${(page - 1) * limit}`;
     } else {
-      sql = ``;
+      sql = `SELECT 
+      id_verif,
+      Creation_date,
+      checker,
+      Driver_out,
+      Driver_in,
+      license_vhc,
+      maintenance FROM gp_verif_vehicule 
+      WHERE id_user = ? 
+      ASC LIMIT ${limit} OFFSET ${(page - 1) * limit}`;
     }
     console.log(sql);
     db.query(sql, callback);
@@ -20,9 +36,9 @@ const Vehiclecheck = {
     let sql;
 
     if (id_user == 1) {
-      sql = `SELECT COUNT(*) as total `;
+      sql = `SELECT COUNT(*) as total FROM gp_verif_vehicule `;
     } else {
-      sql = `SELECT COUNT(*) as total FROM `;
+      sql = `SELECT COUNT(*) as total FROM gp_verif_vehicule WHERE id_user = ?`;
     }
 
     db.query(sql, callback);
