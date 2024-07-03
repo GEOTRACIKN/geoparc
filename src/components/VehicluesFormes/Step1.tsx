f73a1bd820e586a9319c9116e9fe80447da6db12
SBIA Younes <94634321+SBIA-Younes@users.noreply.github.com>
2024-07-03 16:21:22 +0100

    Étape 1 : Finalisation du formulaire d'informations générales
    ✅ J'ai terminé le développement de l'étape 1 (Step1) du formulaire d'ajout de véhicules, qui concerne les informations générales du véhicule.
    
    Dans le fichier `src/components/VehicluesFormes/Step1.tsx`, les fonctionnalités suivantes ont été implémentées :
    
    📋 Champs du formulaire :
    - Immatriculation
    - Acquisition (achat, leasing, location)
    - Catégorie (véhicule automobile, véhicule tractés, autre)
    - État
    - Type (touristique, utilitaire, transport commun, camion, etc.)
    - Type de carburant (essence, gas-oil, GPL, électrique)
    - Marque (récupérée depuis l'API)
    - Modèle
    - Parc (récupéré depuis l'API)
    - Conducteur (récupéré depuis l'API en fonction du parc sélectionné)
    - Affectation du véhicule
    - Gamme
    - Capacité du réservoir
    - Consommation moyenne
    - Codification du véhicule
    - Kilométrage
    - Durée d'amortissement
    
    🔍 Validation des champs :
    La validation des champs a été mise en place pour s'assurer que les informations requises sont correctement remplies avant de passer à l'étape suivante.
    
    🔀 Intégration avec l'API :
    Les données des marques de véhicules, des parcs et des conducteurs sont récupérées depuis l'API pour remplir les listes déroulantes correspondantes.
    
    📈 Expérience utilisateur améliorée :
    Des composants réactifs tels que `InvalidInputFloating` et `SelectorFloating` ont été utilisés pour offrir une meilleure expérience utilisateur lors de la saisie des données.
    
    🚀 Avec cette étape terminée, les utilisateurs peuvent désormais saisir les informations générales du véhicule de manière conviviale et structurée, préparant ainsi le terrain pour les étapes suivantes du processus d'ajout.

--- src/components/VehicluesFormes/Step1.tsx
+++ src/components/VehicluesFormes/Step1.tsx
@@ -1,85 +1,363 @@
-import React, { useState } from 'react';
-import { Form, FormGroup, FormLabel, FormControl, Button, Badge } from 'react-bootstrap';
-interface FormProps {
-  nextStep: () => void;
-  userCallback: (info: any) => void;
-  actionButtons: React.ReactNode;
-}
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Row, Container, Card, Col, FloatingLabel } from 'react-bootstrap';
+import { InvalidInputFloating, SelectorFloating } from './InvalidInput';
+import { 
+  AcquisitionOption, 
+  CategorieOption, 
+  EtatOption, 
+  TypeCarburantOption, 
+  TypeOption,
+  AffectationVehicleOption,
+} from '../../utilities/selectOptions';
+import { 
+  VehicleFormProps, 
+  VehicleFormState, 
+  VehicleSelectOption, 
+  VehicleValidateFormsStep1,
+} from '../../utilities/interfaces';
+import { Bounce, toast } from 'react-toastify';
+import { useTranslate } from '../LanguageProvider';
 
-const Step1: React.FC<FormProps> = ({ nextStep, userCallback, actionButtons }) => {
-  const [info1, setInfo1] = useState<{ [key: string]: string }>({});
+
+
+const backendUrl = 'http://localhost:5000/api/geop';
+const Step1: React.FC<VehicleFormProps> = ({
+  nextStep,
+  userCallback,
+}) => {
+
   const [error, setError] = useState<string>("");
+  // const id_user = localStorage.getItem("GeopUserID");
+  const id_user = 8
+  const { translate } = useTranslate();
+  const [formState, setFormState] = useState<VehicleFormState>(VehicleValidateFormsStep1);
+  const [selectBrand, setSelectBrand] = useState<VehicleSelectOption[]>([]);
+  const [selectParc, setSelectParc] = useState<VehicleSelectOption[]>([]);
+  const [selectDriver, setSelectDriver] = useState<VehicleSelectOption[]>([])
 
-  const onInputChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setInfo1((info1) => ({
-      ...info1,
-      [name]: value,
+  
+  useEffect(() => {
+    const fetchBrandData = async (): Promise<VehicleSelectOption[]> => {
+      const response = await fetch(`${backendUrl}/vehicles/brand`);
+      return await response.json()
+    }
+
+    const fetchParcData = async (): Promise<VehicleSelectOption[]> => {
+      const response = await fetch(`${backendUrl}/vehicles/parc`);
+      return await response.json()
+    }
+    const fetchDriverData = async (): Promise<VehicleSelectOption[]> => {
+      const response = await fetch(`${backendUrl}/Conducteur_contrat/${id_user}`);
+      return await response.json()
+    }
+    
+    
+    const fetchData = async () => {
+      try {
+        const brandData = await fetchBrandData();
+        const parcData = await fetchParcData();
+        const driverData = await fetchDriverData();
+        
+        setSelectBrand(brandData.map((brand: VehicleSelectOption) => ({
+          value: brand.value,
+          label: brand.label
+        })))
+        setSelectParc(parcData.map((parc: VehicleSelectOption) => ({
+          value: parc.value,
+          label: parc.label
+        })))
+        
+        setSelectDriver(driverData.map((drivers:any) => ({
+          value: drivers.id_conducteur,
+          label: `${drivers.nom_conducteur} - ${drivers.prenom_conducteur}`
+        })))
+        // setSelectBrand(combineData);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+      }
+    }
+    fetchData();
+  }, [])  
+
+
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+      validations: {
+        ...prevState.validations,
+        [name]: value.trim() !== "",
+      },
     }));
   };
 
   const validate = () => {
-    if (!info1.name) {
-      setError("Erreur");
+    if (!formState.validations.Immatriculation) {
+      setError("Valide l'Immatriculation");
+    // } else if (!formState.validations.Acquisition ) {
+    //   setError("Valide l'Acquisition ");
+    // } else if (!formState.validations.Categorie) {
+    //   setError("Valide la Catégorie");
+    // } else if (!formState.validations.Etat ) {
+    //   setError("Valide l'Etat ");
     } else {
       setError("");
       nextStep();
-      userCallback(info1);
+      userCallback(formState.values);
     }
   };
 
-  const validInput = (e:any) => {
+  const validInput = (e: any) => {
     console.log(e);
-    
-  }
+  };
 
   return (
-    <div>
-  
+    <div className='w-100'>
       <span style={{ color: "red" }}>{error}</span>
-      <h1 className="text-3xl font-bold underline">
-    Informations Générales
-    </h1>
-      <Form>
-        <FormGroup aria-required='true'>
-          <FormLabel aria-required='true' >Immatriculation : </FormLabel>
-          <FormControl
-            type="text"
-            name="name"
-            placeholder="Immatriculation"
-            onChange={onInputChanged}
-            className="input input-bordered is-invalid"
-            onChangeCapture={validInput}
+      <h2 className="text-3xl font-bold underline">Informations Générales</h2>
+      <Form onSubmit={(e) => e.preventDefault()}>
+        <Row className='w-full'>
+          <InvalidInputFloating
+            className='col-md-6'
+            label="Immatriculation :"
+            name="Immatriculation"
+            placeholder=" "
+            value={formState.values.Immatriculation}
+            onChange={handleChange}
+            isValid={formState.validations.Immatriculation}
+            errorMessage="Ce champ est obligatoire."
+            showValid={true}
           />
-        </FormGroup>
-        <br />
-        <FormGroup>
-          <FormLabel>Nom d'utilisateur : </FormLabel>
-          <FormControl
-            type="text"
-            name="staff"
-            placeholder="Nom d'utilisateur"
-            onChange={onInputChanged}
-            className="input input-bordered"
+          <SelectorFloating
+            className='col-md-6'
+            label="Acquisition"
+            name="Acquisition"
+            value={formState.values.Acquisition}
+            onChange={handleChange}
+            isValid={formState.validations.Acquisition}
+            errorMessage="Ce champ est obligatoire."
+            options={AcquisitionOption}
+            showValid={true}
           />
-        </FormGroup>
-        <br />
-        <FormGroup>
-          <FormLabel>Email : </FormLabel>
-          <FormControl
-            type="text"
-            name="address"
-            placeholder="Email"
-            onChange={onInputChanged}
-            className="input input-bordered"
+        </Row>
+        <Row className='w-full'>
+          <SelectorFloating
+            className='col-md-6'
+            label="Catégorie"
+            name="Categorie"
+            value={formState.values.Categorie}
+            onChange={handleChange}
+            isValid={formState.validations.Categorie}
+            errorMessage="Ce champ est obligatoire."
+            options={CategorieOption}
+            showValid={true}
           />
-        </FormGroup>
-        <br />
+          <SelectorFloating
+            className='col-md-6'
+            label="Etat"
+            name="Etat"
+            value={formState.values.Etat}
+            onChange={handleChange}
+            isValid={formState.validations.Etat}
+            errorMessage="Ce champ est obligatoire."
+            options={EtatOption}
+            showValid={true}
+          />
+        </Row>
+        <Row className='w-full'>
+          <SelectorFloating
+            className='col-md-6'
+            label="Type"
+            name="Type"
+            value={formState.values.Type}
+            onChange={handleChange}
+            isValid={formState.validations.Type}
+            errorMessage="Ce champ est obligatoire."
+            options={TypeOption}
+            showValid={true}
+          />
+          <SelectorFloating
+            className='col-md-6'
+            label="Type carburant"
+            name="TypeCarburant"
+            value={formState.values.TypeCarburant}
+            onChange={handleChange}
+            isValid={formState.validations.TypeCarburant}
+            errorMessage="Ce champ est obligatoire."
+            options={TypeCarburantOption}
+            showValid={true}
+          />
+        </Row>
+        <Row className='w-full'>
+          <SelectorFloating
+            className='col-md-6'
+            label="Marque"
+            name="Marque"
+            value={formState.values.Marque}
+            onChange={handleChange}
+            isValid={formState.validations.Marque}
+            errorMessage="Ce champ est obligatoire."
+            options={selectBrand}
+            showValid={true}
+          />
+          <InvalidInputFloating
+            className='col-md-6'
+            label="Modèle :"
+            name="Modele"
+            placeholder=" "
+            value={formState.values.Modele}
+            onChange={handleChange}
+            isValid={formState.validations.Modele}
+            errorMessage="Ce champ est obligatoire."
+            showValid={true}
+          />
+        </Row>
+        <Row className='w-full'>
+          <Form.Group controlId='formBasicSelect-Parc' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Parc'>
+            <Form.Select
+              as="select"
+              name='Parc'
+              value={formState.values.Parc}
+              onChange={(e) => handleChange(e)}
+              className={formState.validations.Parc ? 'is-valid' : ''}
+              >
+              <option value="">Sélectionnez une option</option>
+              {selectParc.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group controlId='formBasicSelect-driver' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Conducteur'>
+            <Form.Select
+              as="select"
+              name='Driver'
+              value={formState.values.Driver}
+              onChange={(e) => handleChange(e)}
+              className={formState.validations.Driver ? 'is-valid' : ''}
+              >
+              <option value="">Sélectionnez une option</option>
+              {selectDriver.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
+            </FloatingLabel>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group controlId='formBasicSelect-AffectationVehicl' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Affectation Vehicule'>
+            <Form.Select
+              as="select"
+              name='AffectationVehicl'
+              value={formState.values.AffectationVehicl}
+              onChange={(e) => handleChange(e)}
+              className={formState.validations.AffectationVehicl ? 'is-valid' : ''}
+              >
+              <option value="">Sélectionnez une option</option>
+              {AffectationVehicleOption.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group controlId='formBasicInput-Gamme' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Gamme'>
+            <Form.Control
+            placeholder=' '
+            type="text"
+            name='Gamme'
+            value={formState.values.Gamme}
+            onChange={(e:any) => handleChange(e)}
+            className={formState.validations.Gamme ? 'is-valid' : ''}
+            />
+          </FloatingLabel>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group controlId='formBasicInput-Gamme' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Capacité réservoir (L)'>
+            <Form.Control
+            placeholder=' '
+            type="text"
+            name='Capacite_res'
+            value={formState.values.Capacite_res}
+            onChange={(e:any) => handleChange(e)}
+            className={formState.validations.Capacite_res ? 'is-valid' : ''}
+            />
+          </FloatingLabel>
+          </Form.Group>
+          <Form.Group controlId='formBasicInput-consom_moy' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Consommation moyenne (l/100km)s'>
+            <Form.Control
+            placeholder=' '
+            type="text"
+            name='Consom_moy'
+            value={formState.values.Consom_moy}
+            onChange={(e:any) => handleChange(e)}
+            className={formState.validations.Consom_moy ? 'is-valid' : ''}
+            />
+          </FloatingLabel>
+          </Form.Group>
+        </Row>
+        <Row>
+          <InvalidInputFloating
+            className='col-md-6'
+            label="Codification véhicule :"
+            name="Codification"
+            placeholder=" "
+            value={formState.values.Codification}
+            onChange={handleChange}
+            isValid={formState.validations.Codification}
+            errorMessage="Ce champ est obligatoire."
+            showValid={true}
+          />
+          <Form.Group controlId='formBasicInput-Mileage' className='mt-2 col-md-6'>
+            <FloatingLabel controlId="floatingSelect" label='Kilométrage (Km)'>
+            <Form.Control
+            placeholder=' '
+            type="text"
+            name='Mileage'
+            value={formState.values.Mileage}
+            onChange={(e:any) => handleChange(e)}
+            className={formState.validations.Mileage ? 'is-valid' : ''}
+            />
+          </FloatingLabel>
+          </Form.Group>
+        </Row>
+        <Row>
+          <InvalidInputFloating
+            className='col-md-6'
+            label="Durée d'amortissement (jours) :"
+            name="Payback_Period"
+            placeholder=" "
+            value={formState.values.Payback_Period}
+            onChange={handleChange}
+            isValid={formState.validations.Payback_Period}
+            errorMessage="Ce champ est obligatoire."
+            showValid={true}
+          />
+          
+        </Row>
+  
         <Button variant="primary" onClick={validate}>
           Soumettre
         </Button>
       </Form>
-      
     </div>
   );
 };
