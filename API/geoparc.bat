a5e2de44d0e8afc77ae98a9d7b5130c22e606b11
webanalytis <webanalytis@gmail.com>
2024-08-05 12:01:09 +0100

    update

--- API/geoparc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-tasklist /fi "imagename eq node.exe" | find ":" > nul
-if errorlevel 1 (
-    echo Starting geoparc.js...
-    %windir%\system32\CMD.exe /K "pm2 stop geoparc.js --geoparc --silent & pm2 start geoparc.js --geoparc --silent geoparc & pm2 flush geot --silent"
-) else (
-    echo geoparc.js is already running.
-)
