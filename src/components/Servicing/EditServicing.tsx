611c7c2d48f76426985e56aabea67acbeafb7e50
ines_benachenhou <103831105+inesbenachenhou@users.noreply.github.com>
2024-10-22 12:48:14 +0200

    front update

--- src/components/Servicing/EditServicing.tsx
+++ src/components/Servicing/EditServicing.tsx
@@ -175,6 +175,40 @@ const ModalEditServicing: React.FC<ModalEditServicingProps> = ({
                     style={{ maxHeight: "calc(80vh - 200px)", overflowY: "auto" }}
                 >
 
+                    
+
+<Form.Group controlId="type_vehicule">
+                        <Form.Label>{translate("Vehicle Type")}</Form.Label>
+                        <Form.Control
+                            type="text"
+                            readOnly={!isEditable}
+                            //placeholder="Entrez le véhicule"
+                            value={formData.type_vehicule}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="date_servicing">
+                        <Form.Label>{translate("Request Date")}</Form.Label>
+                        <Form.Control
+                            type="datetime-local"
+                            readOnly={!isEditable}
+                            value={formData.date_servicing}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+
+                    
+                    <Form.Group controlId="km_servicing">
+                        <Form.Label>{translate("KM")}</Form.Label>
+                        <Form.Control
+                            type="number"
+                            readOnly={!isEditable}
+                            //placeholder="Entrez le kilométrage"
+                            value={formData.km_servicing}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+
 
         <Form.Group controlId="invoice_no_servicing">
                         <Form.Label>{translate("Invoice No")}</Form.Label>
@@ -210,26 +244,6 @@ const ModalEditServicing: React.FC<ModalEditServicingProps> = ({
 
                    
 
-                    <Form.Group controlId="type_vehicule">
-                        <Form.Label>{translate("Vehicle Type")}</Form.Label>
-                        <Form.Control
-                            type="text"
-                            readOnly={!isEditable}
-                            //placeholder="Entrez le véhicule"
-                            value={formData.type_vehicule}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="date_servicing">
-                        <Form.Label>{translate("Request Date")}</Form.Label>
-                        <Form.Control
-                            type="datetime-local"
-                            readOnly={!isEditable}
-                            value={formData.date_servicing}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-
                     <Form.Group controlId="place_servicing">
                         <Form.Label>{translate("Place")}</Form.Label>
                         <Form.Control
@@ -259,16 +273,6 @@ const ModalEditServicing: React.FC<ModalEditServicingProps> = ({
                         />
                     </Form.Group>
                    
-                    <Form.Group controlId="km_servicing">
-                        <Form.Label>{translate("KM")}</Form.Label>
-                        <Form.Control
-                            type="number"
-                            readOnly={!isEditable}
-                            //placeholder="Entrez le kilométrage"
-                            value={formData.km_servicing}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
 
                     <Form.Group controlId="next_oil_change_servicing">
                         <Form.Label>{translate("Next oil change (days)")}</Form.Label>
--- src/components/Servicing/ShowServicing.tsx
+++ src/components/Servicing/ShowServicing.tsx
@@ -117,6 +117,38 @@ const ModalShowServicing: React.FC<ModalShowServicingnProps> = ({
                 <Modal.Body
                     style={{ maxHeight: "calc(80vh - 200px)", overflowY: "auto" }}
                 >
+                      <Form.Group controlId="type_vehicule">
+                        <Form.Label>{translate("Vehicle Type")}</Form.Label>
+                        <Form.Control
+                            type="text"
+                            readOnly={!isEditable}
+
+                            //placeholder="Entrez le véhicule"
+                            value={formData.type_vehicule}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="date_servicing">
+                        <Form.Label>{translate("Request Date")}</Form.Label>
+                        <Form.Control
+                            type="datetime-local"
+                            readOnly={!isEditable}
+
+                            value={formData.date_servicing}
+                            onChange={handleChange}
+                        />
+                          <Form.Group controlId="km_servicing">
+                        <Form.Label>{translate("KM")}</Form.Label>
+                        <Form.Control
+                            type="number"
+                            readOnly={!isEditable}
+
+                            //placeholder="Entrez le kilométrage"
+                            value={formData.km_servicing}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    </Form.Group>
                       <Form.Group controlId="invoice_no_servicing">
                         <Form.Label>{translate("Invoice No")}</Form.Label>
                         <Form.Control
@@ -143,27 +175,7 @@ const ModalShowServicing: React.FC<ModalShowServicingnProps> = ({
 
                    
 
-                    <Form.Group controlId="type_vehicule">
-                        <Form.Label>{translate("Vehicle Type")}</Form.Label>
-                        <Form.Control
-                            type="text"
-                            readOnly={!isEditable}
-
-                            //placeholder="Entrez le véhicule"
-                            value={formData.type_vehicule}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="date_servicing">
-                        <Form.Label>{translate("Request Date")}</Form.Label>
-                        <Form.Control
-                            type="datetime-local"
-                            readOnly={!isEditable}
-
-                            value={formData.date_servicing}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+                  
 
                     <Form.Group controlId="place_servicing">
                         <Form.Label>{translate("Place")}</Form.Label>
@@ -200,17 +212,7 @@ const ModalShowServicing: React.FC<ModalShowServicingnProps> = ({
                         />
                     </Form.Group>
                    
-                    <Form.Group controlId="km_servicing">
-                        <Form.Label>{translate("KM")}</Form.Label>
-                        <Form.Control
-                            type="number"
-                            readOnly={!isEditable}
-
-                            //placeholder="Entrez le kilométrage"
-                            value={formData.km_servicing}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+                  
 
                     <Form.Group controlId="next_oil_change_servicing">
                         <Form.Label>{translate("Nex oil change")}</Form.Label>
